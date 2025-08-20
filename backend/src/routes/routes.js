e3ca86eab62d58b0c6e7c0f5e115d8cba788ce5a
Paing Soe Khant <babyboss7916@gmail.com>
2025-08-20 20:09:21 +0630

    edit server

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -14,5 +14,4 @@ router.put("/:id",updateNote);
 
 router.delete("/:id",deleteNote);
 
-
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,21 +4,37 @@ import dotenv from "dotenv";
 import {connectDB} from "./config/db.js";
 import rateLimiter from "./middleware/rateLimits.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config(); //.env file config
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+// const __dirname = path.resolve();
+
+// if(process.env.NODE_ENV !== "production") {
+//
+//
+// }
 
 app.use(cors({
     origin: "http://localhost:5173",
 }));
+
 //middleware
 app.use(express.json());//this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
 
 app.use("/api/notes",routes);
+
+// if(process.env.NODE_ENV === "production") {
+//
+//     app.use(express.static(path.join(__dirname,"../frontend/dist")));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+//     });
+// }
 
 await connectDB().then(()=>{
     app.listen(PORT,()=>{
